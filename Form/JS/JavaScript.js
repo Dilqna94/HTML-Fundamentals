d7b140917e8293332edba20b045aa318bc6818ec
Dilqna94 <dilyanadim94@gmail.com>
2015-04-28 14:52:53 +0300

    JS_Form

---
+++ Form/JS/JavaScript.js
@@ -0,0 +1,28 @@
+﻿function validate(myForm)
+{
+    var booValid = true;
+    var ErrorMessage = " ";
+  
+    //if (myForm.ID.value == "") {
+    //    ErrorMessage += "ID field cannot be empty\n";
+    //    booValid = false;
+    //}
+    if (myForm.country.selectedIndex == 0) {
+        ErrorMessage += "select country please! \n";
+        booValid = false;
+    }
+ 
+
+    if ((!myForm.radio[0].checked) && (!myForm.radio[1].checked)) {
+        ErrorMessage += "please select radiobutton \n";
+        booValid = false;
+    }
+    if ((!myForm.check[0].checked) && (!myForm.check[1].checked)) {
+        ErrorMessage += "Please check your language \n";
+        booValid = false;
+    }
+    if (!booValid) {
+        alert(ErrorMessage);
+    }
+    return booValid;
+}
